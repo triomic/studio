74a018eb95f226888c980f2b1b64049a6866691d
Sean Lim <seanlimdev856@gmail.com>
2022-03-19 19:51:23 +0800

    remove storybook

--- .storybook/main.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  stories: ["../src/**/*.stories.tsx"],
-  addons: [
-    "@storybook/addon-actions",
-    "@storybook/addon-docs",
-    "@storybook/addon-controls",
-    "@storybook/addon-viewport",
-  ],
-};
--- .storybook/preview.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export const parameters = {
-  controls: { expanded: true },
-};
-export const decorators = [
-  (storyFn) => <div style={{ padding: "16px" }}>{storyFn()}</div>,
-];
